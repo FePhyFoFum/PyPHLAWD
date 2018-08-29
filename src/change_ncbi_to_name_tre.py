329c256c42fb5f394a86314dc46ee9c3bb7d84a3
Joseph Brown <phylo.jwb@gmail.com>
2018-08-29 08:25:01 +0100

    no replace spaces if quoted

--- src/change_ncbi_to_name_tre.py
+++ src/change_ncbi_to_name_tre.py
@@ -17,11 +17,13 @@ if __name__ == "__main__":
     for i in tree_reader.read_tree_file_iter(sys.argv[2]):
         for j in i.iternodes():
             if j.label in idn:
-                lab = idn[j.label].replace(" ","_")
+                lab = idn[j.label]
                 # check if quotes are required bc of invalid chars
                 if any(elem in lab for elem in invalidchars):
                     #print "gotta quote this sucka: " + lab
                     lab = "\"" + lab + "\""
+                else:
+                    lab = lab.replace(" ","_")
                 j.label = lab
         outf.write(i.get_newick_repr(True)+";")
     outf.close()
