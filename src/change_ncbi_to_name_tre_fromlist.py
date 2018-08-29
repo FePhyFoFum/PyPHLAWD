1a655350c69a04566d0a39d2f2712f5d6267aed0
Joseph Brown <phylo.jwb@gmail.com>
2018-08-29 08:29:10 +0100

    newick quoting for other filez

--- src/change_ncbi_to_name_tre_fromlist.py
+++ src/change_ncbi_to_name_tre_fromlist.py
@@ -8,6 +8,7 @@ if __name__ == "__main__":
         sys.exit(0)
     tab = open(sys.argv[1],"r")
     idn = {}
+    invalidchars = ":;[](),"
     for i in tab:
         spls = i.strip().split("\t")
         idn[spls[0]] = spls[3]
@@ -16,7 +17,14 @@ if __name__ == "__main__":
     for i in tree_reader.read_tree_file_iter(sys.argv[2]):
         for j in i.iternodes():
             if j.label in idn:
-                j.label = idn[j.label].replace(" ","_")
+                lab = idn[j.label]
+                # check if quotes are required bc of invalid chars
+                if any(elem in lab for elem in invalidchars):
+                    #print "gotta quote this sucka: " + lab
+                    lab = "\"" + lab + "\""
+                else:
+                    lab = lab.replace(" ","_")
+                j.label = lab
         outf.write(i.get_newick_repr(True)+";")
     outf.close()
    
--- src/change_ncbi_to_ott_tre_for_treemerge.py
+++ src/change_ncbi_to_ott_tre_for_treemerge.py
@@ -38,12 +38,20 @@ if __name__ == "__main__":
                 ncbis.append(j.label)
 
     idn = get_ott_names_for_ncbi(sys.argv[1], ncbis)
+    invalidchars = ":;[](),"
 
     outf = open(sys.argv[3],"w")
     for i in tree_reader.read_tree_file_iter(sys.argv[2]):
         for j in i.iternodes():
             if j.label in idn:
-                j.label = idn[j.label].replace(" ","_")
+                lab = idn[j.label]
+                # check if quotes are required bc of invalid chars
+                if any(elem in lab for elem in invalidchars):
+                    #print "gotta quote this sucka: " + lab
+                    lab = "\"" + lab + "\""
+                else:
+                    lab = lab.replace(" ","_")
+                j.label = lab
         outf.write(i.get_newick_repr(True)+";")
     outf.close()
    
