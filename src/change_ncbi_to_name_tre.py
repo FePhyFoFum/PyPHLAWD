c494a77f42288288993ca8a166e7717edf4992cb
Joseph W. Brown <phylo.jwb@gmail.com>
2019-03-26 14:22:46 +0000

    argparse tree renamer

--- src/change_ncbi_to_name_tre.py
+++ src/change_ncbi_to_name_tre.py
@@ -2,11 +2,23 @@ import sys
 import tree_reader
 import os
 from utils import newick_name
+import argparse as ap
+
+def generate_argparser():
+    parser = ap.ArgumentParser(prog="change_ncbi_to_name_fasta.py",
+        formatter_class=ap.ArgumentDefaultsHelpFormatter)
+    parser = ap.ArgumentParser()
+    parser.add_argument("-t", "--table", type=str, help="NCBI translation table", required=True)
+    parser.add_argument("-i", "--infile", type=str, help="Input fasta alignment", required=True)
+    parser.add_argument("-o", "--outfile", type=str, help="Output fasta alignment", required=True)
+    return parser
 
 if __name__ == "__main__":
-    if len(sys.argv) != 4:
-        print("python "+sys.argv[0]+" table infile outfile")
-        sys.exit(0)
+    parser = generate_argparser()
+    if len(sys.argv[1:]) == 0:
+        sys.argv.append("-h")
+    args = parser.parse_args(sys.argv[1:])
+    
     tab = open(sys.argv[1],"r")
     idn = {}
     for i in tab:
@@ -20,4 +32,3 @@ if __name__ == "__main__":
                 j.label = newick_name(idn[j.label])
         outf.write(i.get_newick_repr(True)+";")
     outf.close()
-   
